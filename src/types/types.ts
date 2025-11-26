83f15f0070ae9ba0cca8970b2fcf81712ff32e9c
MarcinMove37ai <marcinlisiak83@gmial.com>
2025-11-26 19:24:10 +0100

    fixed docker_free2free_ver_UserRoleFIX

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,4 +1,4 @@
-export type UserRole = 'ADMIN' | 'USER' | 'GOD' | 'payd' | 'free' | 'free_ver' | 'rookie' | 'creator' | 'unlimited';
+export type UserRole = 'ADMIN' | 'USER' | 'GOD' | 'payd' | 'free' | 'free_ver' | 'rookie' | 'creator' | 'unlimited'  'demo' ;
 
 export interface User {
   id: string;
